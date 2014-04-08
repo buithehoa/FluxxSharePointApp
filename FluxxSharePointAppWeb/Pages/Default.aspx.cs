03529729312314e68724e47f7b200e6798efadeb
Bui The Hoa <buithehoa@gmail.com>
2014-04-08 16:14:02 +0700

    Obtain app-only access token

--- FluxxSharePointAppWeb/Pages/Default.aspx.cs
+++ FluxxSharePointAppWeb/Pages/Default.aspx.cs
@@ -5,23 +5,12 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace FluxxSharePointAppWeb.Pages
-{
-    public partial class Default : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            // The following code gets the client context and Title property by using TokenHelper.
-            // To access other properties, you may need to request permissions on the host web.
-
-            Uri hostWeb = new Uri(Request.QueryString["SPHostUrl"]);
-
-            using (var clientContext = TokenHelper.GetS2SClientContextWithWindowsIdentity(hostWeb, Request.LogonUserIdentity))
-            {
-                clientContext.Load(clientContext.Web, web => web.Title);
-                clientContext.ExecuteQuery();
-                Response.Write(clientContext.Web.Title);
-            }
-        }
+namespace FluxxSharePointAppWeb.Pages {
+  public partial class Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+      Uri hostWeb = new Uri("https://fluxxtest.packard.org/");
+      string appOnlyAccessToken = TokenHelper.GetS2SAccessTokenWithWindowsIdentity(hostWeb, null);
+      Response.Write(appOnlyAccessToken);
     }
+  }
 }
